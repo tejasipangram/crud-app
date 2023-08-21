9cc4734eb84a6b86b75e5527663e90179c70b6df
tejasipangram <tejasgiri@ipangram.com>
2023-08-21 14:57:36 +0530

    fixed bug in update

--- backend/crud-backend-master/controller/update.js
+++ backend/crud-backend-master/controller/update.js
@@ -5,41 +5,56 @@ export const udpate = async (req, res) => {
     const id = req.params.id;
     const { title, description } = req.body;
 
-    //checking file size
-    if (req.file.size > 5 * 1024 * 1024) {
-      return res.status(500).json({
-        success: false,
-        message: "You can not upload file greater than 5 mb",
-      });
-    }
+    if (req.file) {
+      //checking file size
+      if (req.file.size > 5 * 1024 * 1024) {
+        return res.status(500).json({
+          success: false,
+          message: "You can not upload file greater than 5 mb",
+        });
+      }
 
-    let fileType = req.file.originalname.split(".");
-    fileType = fileType[fileType.length - 1];
-    console.log(fileType);
-    if (fileType !== "png" && fileType !== "jpg") {
-      console.log(req.file.mimetype.split("/")[1]);
-      return res.status(500).json({
-        success: false,
-        message: "Only jpg or png files are allowed",
-      });
-    }
+      let fileType = req.file.originalname.split(".");
+      fileType = fileType[fileType.length - 1];
+      console.log(fileType);
+      if (fileType !== "png" && fileType !== "jpg") {
+        console.log(req.file.mimetype.split("/")[1]);
+        return res.status(500).json({
+          success: false,
+          message: "Only jpg or png files are allowed",
+        });
+      }
 
-    const filePath = req.fileName;
-    const data = await List.findByIdAndUpdate(
-      id,
-      {
-        title,
-        description,
-        filePath,
-      },
-      { new: true }
-    );
+      const filePath = req.fileName;
+    }
+    let data;
+    if (req.file) {
+      data = await List.findByIdAndUpdate(
+        id,
+        {
+          title,
+          description,
+          filePath,
+        },
+        { new: true }
+      );
+    } else {
+      data = await List.findByIdAndUpdate(
+        id,
+        {
+          title,
+          description,
+        },
+        { new: true }
+      );
+    }
     await data.save();
 
     return res
       .status(200)
       .json({ success: true, message: "List updated successfully", data });
   } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .json({ success: false, message: "Something went wrong " });
